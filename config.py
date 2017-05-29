fcf77fbfe19b28a82f4670f78760be779e923fc9
nikolai <nikoren@gmail.com>
2017-05-28 23:09:01 -0400

    Update config

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ class DevelopmentConfig(Config):
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'nikoren2safari'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'safari11safari@'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
     #                           'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/{}'.format(Config.PROJECT_NAME.lower())
